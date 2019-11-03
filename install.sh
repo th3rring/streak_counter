cc33d2cc082ed6194e9cacf35a6a4e9a25e5bb92
Thomas Herring <teh6@rice.edu>
2019-11-03 01:42:48 -0500

    Added install script

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+wget http://www.airspayce.com/mikem/bcm2835/bcm2835-1.60.tar.gz
+tar zxvf bcm2835-1.60.tar.gz 
+cd bcm2835-1.60/
+sudo ./configure
+sudo make
+sudo make check
+sudo make install
+sudo apt-get install wiringpi
+sudo apt-get update
+sudo apt-get install python3-pip
+sudo apt-get install python3-pil
+sudo apt-get install python3-numpy
+sudo pip3 install RPi.GPIO
+sudo pip3 install spidev
+sudo pip3 -r requirements.txt
